c65356a3b7b9a0f43963fbe827962a6069d0f1db
inti <inti.albuquerque@trademe.co.nz>
2023-04-19 15:35:33 +1200

    Initial Commit

---
+++ SupermarketSpecialsApp/SupermarketSpecialsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupermarketSpecialsAppApp.swift
+//  SupermarketSpecialsApp
+//
+//  Created by ialbuquerque on 19/04/23.
+//
+
+import SwiftUI
+
+@main
+struct SupermarketSpecialsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
